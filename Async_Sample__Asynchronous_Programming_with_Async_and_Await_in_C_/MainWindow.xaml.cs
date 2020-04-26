0b2f5f931129ee7fd07d61eb1c7fc08a3d5f9b9d
lxy <lxysac@foxmail.com>
2020-04-26 17:03:37 +0800

    异步窗体

---
+++ Async_Sample__Asynchronous_Programming_with_Async_and_Await_in_C_/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+
+// Add a using directive and a reference for System.Net.Http;
+using System.Net.Http;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AsyncFirstExample
+{
+    public partial class MainWindow : Window
+    {
+        CancellationTokenSource cts;
+        private Task pendingWork = null;
+        private char group = (char)('A' - 1);
+        private List<string> SetUpURLList()
+        {
+            List<string> urls = new List<string>
+            {
+                "https://msdn.microsoft.com",
+                "https://msdn.microsoft.com/library/hh290138.aspx",
+                "https://msdn.microsoft.com/library/hh290140.aspx",
+                "https://msdn.microsoft.com/library/dd470362.aspx",
+                "https://msdn.microsoft.com/library/aa578028.aspx",
+                "https://msdn.microsoft.com/library/ms404677.aspx",
+                "https://msdn.microsoft.com/library/ff730837.aspx"
+            };
+            return urls;
+        }
+        private async void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            startButton.IsEnabled = false;
+            group = (char)(group + 1);
+            resultsTextBox.Text += $"\r\n\r\n#Starting group {group}.";
+
+            if (cts != null)
+            {
+                cts.Cancel();
+            }
+            CancellationTokenSource newCTS = new CancellationTokenSource();
+            cts = newCTS;//将 cts 设置为表示当前进程的不同值。
+            //resultsTextBox.Clear();
+            try
+            {
+
+                //在一段时间后取消
+                //cts.CancelAfter(2500);
+                char finishedGroup = await AccessTheWebAsync(group);
+                resultsTextBox.Text += $"\r\n\r\n#Group {finishedGroup} is complete.\r\n";
+            }
+            catch (OperationCanceledException)
+            {
+                resultsTextBox.Text += "\r\nDownload canceled.\r\n";
+            }
+            catch (Exception)
+            {
+                resultsTextBox.Text += "\r\nDownload failed.\r\n";
+            }
+            finally
+            {
+                startButton.IsEnabled = true;
+            }
+            if (cts == newCTS)
+                cts = null;
+        }
+        async Task<int> ProcessURLAsync(string url, HttpClient client, CancellationToken ct)
+        {
+            HttpResponseMessage response = await client.GetAsync(url, ct);
+            byte[] urlContents = await response.Content.ReadAsByteArrayAsync();
+            return urlContents.Length;//下载完成返回大小
+        }
+        async Task AccessTheWebAsync(CancellationToken ct)
+        {
+            HttpClient client = new HttpClient();
+
+            IEnumerable<Task<int>> downloadTasksQuery =from url in SetUpURLList() select ProcessURLAsync(url, client, ct);
+
+            List<Task<int>> downloadTasks = downloadTasksQuery.ToList();//所有下载任务开始
+
+            while (downloadTasks.Count > 0)//下载数>0进行下载
+            {
+                Task<int> firstFinishedTask = await Task.WhenAny(downloadTasks);//执行若干个任务，只需要对其中任意一个的完成进行响应。这主要用于：对一个操作进行多种独立的尝试，只要一个尝试完成，任务就算完成。例如，同时向多个 Web 服务询问股票价格，但是只关心第一个响应的。
+
+                downloadTasks.Remove(firstFinishedTask);//下载完成移除任务
+
+                int length = await firstFinishedTask;
+                resultsTextBox.Text += $"\r\nLength of the download:  {length}";
+            }
+        }
+        private async Task<char> AccessTheWebAsync(char grp)
+        {
+            HttpClient client = new HttpClient();
+            List<string> urlList = SetUpURLList();
+            Task<byte[]>[] getContentTasks = urlList.Select(url => client.GetByteArrayAsync(url)).ToArray();
+            pendingWork = FinishOneGroupAsync(urlList, getContentTasks, grp);
+            resultsTextBox.Text += $"\r\n#Task assigned for group {grp}. Download tasks are active.\r\n";
+            await pendingWork;
+            return grp;
+        }
+        private async Task FinishOneGroupAsync(List<string> urls, Task<byte[]>[] contentTasks, char grp)
+        {
+            if (pendingWork != null) await pendingWork;
+            int total = 0;
+            for (int i = 0; i < contentTasks.Length; i++)
+            {
+                byte[] content = await contentTasks[i];
+                DisplayResults(urls[i], content, i, grp);
+                total += content.Length;
+            }
+            resultsTextBox.Text +=
+                $"\r\n\r\nTOTAL bytes returned:  {total}\r\n";
+        }
+
+        private void DisplayResults(string v, byte[] content, int i, char grp)
+        {
+            resultsTextBox.Text += $"\r\n{grp}-{i}:{v}\t=>大小：{content.Length}";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cts!=null)
+            {
+                cts.Cancel();
+            }
+        }
+    }
+}
--- ConsoleApp_Task/Program.cs
+++ ConsoleApp_Task/Program.cs
@@ -13,14 +13,82 @@ namespace ConsoleApp_Task
      * 要将同步解决方案转换为异步解决方案，
      * 最佳着手点在 GetURLContents 中，因为对 HttpWebRequest 方法 GetResponse 的调用以及对 Stream 方法 CopyTo 的调用是应用程序访问 Web 的位置。 
      * 
+     * ValueTask<int> :Task 和 Task<TResult> 是引用类型，因此，性能关键路径中的内存分配会对性能产生负面影响，尤其当分配出现在紧凑循环中时。 支持通用返回类型意味着可返                  回轻量值类型（而不是引用类型），从而避免额外的内存分配。通过.Result属性获取结果
+     * 
+     * IAsyncEnumerable<string：可等待异步流。 异步流提供了一种方法，来枚举在具有重复异步调用的块中生成元素时从流中读取的项。
+     * 
+     * Void ：在异步事件处理程序中使用 void 返回类型，这需要 void 返回类型。 对于事件处理程序以外的不返回值的方法，应返回 Task，因为无法等待返回 void 的异步方法。 此类        方法的任何调用方都必须继续完成，而无需等待调用的异步方法完成。 调用方必须独立于异步方法生成的任何值或异常。
+               返回 void 的异步方法的调用方无法捕获从该方法引发的异常，且此类未经处理的异常可能会导致应用程序故障。 如果返回 Task 或 Task<TResult> 的方法引发异常，则该异常存储在返回的任务中。 等待任务时，将重新引发异常。 因此，请确保可以产生异常的任何异步方法都具有返回类型 Task 或 Task<TResult>，并确保会等待对方法的调用。
+     * 
+     * Task：不包含 return 语句的异步方法或包含不返回操作数的 return 语句的异步方法通常具有返回类型 Task。 如果此类方法同步运行，它们将返回 void。 如果在异步方法中使用       Task 返回类型，调用方法可以使用 await 运算符暂停调用方的完成，直至被调用的异步方法结束。
+     * 
+     * Task<TResult> ：返回类型用于某种异步方法，此异步方法包含 return (C#) 语句，其中操作数是 TResult。
+     * FromResult 异步方法是返回字符串的操作的占位符。
      **/
     class Program
     {
+
+        static TaskCompletionSource<bool> tcs = new TaskCompletionSource<bool>();
+        static Random? rnd;
         static async Task Main(string[] args)
         {
-            await SumPageSizesAsync();
+            tcs = new TaskCompletionSource<bool>(); 
+            var secondHandlerFinished = tcs.Task;
+
+            var button = new NaiveButton();
+            button.Clicked += Button_Clicked_1;
+            button.Clicked += Button_Clicked_2_Async;
+            button.Clicked += Button_Clicked_3;
+
+            Console.WriteLine("About to click a button...");
+            button.Click();
+            Console.WriteLine("Button's Click method returned.");
+
+            await secondHandlerFinished;
+            var today = await Task.FromResult(DateTime.Now.DayOfWeek.ToString());
+            //await SumPageSizesAsync();
+
             //Console.WriteLine(msg);
         }
+        private static async IAsyncEnumerable<string> ReadWordsFromStream()
+        {
+            string data =
+            @"This is a line of text.
+      Here is the second line of text.
+      And there is one more for good measure.
+      Wait, that was the penultimate line.";
+
+            using var readStream = new StringReader(data);
+
+            string? line = await readStream.ReadLineAsync();
+            while (line != null)
+            {
+                var words = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                foreach (var word in words)
+                {
+                    yield return word;
+                }
+                line = await readStream.ReadLineAsync();
+            }
+        }
+        private static async ValueTask<int> GetDiceRoll()
+        {
+            Console.WriteLine("...Shaking the dice...");
+            int roll1 = await Roll();
+            int roll2 = await Roll();
+            return roll1 + roll2;
+        }
+
+        private static async ValueTask<int> Roll()
+        {
+            if (rnd == null)
+                rnd = new Random();
+
+            await Task.Delay(500);
+            int diceRoll = rnd.Next(1, 7);
+            return diceRoll;
+        }
+
         private async static Task SumPageSizesAsync()
         {
             // string msg = "";
@@ -41,6 +109,12 @@ namespace ConsoleApp_Task
             IEnumerable<Task<int>> downloadTasksQuery =from url in SetUpURLList() select ProcessURLAsync(url);
             Task<int>[] downloadTasks = downloadTasksQuery.ToArray();
             Console.WriteLine("do something。。。");
+            Console.WriteLine($"You rolled {GetDiceRoll().Result}");
+            IAsyncEnumerable<string> list = ReadWordsFromStream();
+            await foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
 
             int[] lengths = await Task.WhenAll(downloadTasks);
             int total = lengths.Sum();
@@ -178,6 +252,45 @@ namespace ConsoleApp_Task
 
             int total = length1 + length2 + length3;
             Console.WriteLine($"\r\n\r\nTotal bytes returned:  {total}\r\n");
+        }
+
+        private static void Button_Clicked_1(object? sender, EventArgs e)
+        {
+            Console.WriteLine("   Handler 1 is starting...");
+            Task.Delay(100).Wait();
+            Console.WriteLine("   Handler 1 is done.");
+        }
+
+        private static async void Button_Clicked_2_Async(object? sender, EventArgs e)
+        {
+            Console.WriteLine("   Handler 2 is starting...");
+            Task.Delay(100).Wait();
+            Console.WriteLine("   Handler 2 is about to go async...");
+            //这里等待时，事件3已经触发
+            await Task.Delay(500);
+            Console.WriteLine("   Handler 2 is done.");
+            //告诉主线程事件已经完成
+            tcs.SetResult(true);
+        }
+
+        private static void Button_Clicked_3(object? sender, EventArgs e)
+        {
+            Console.WriteLine("   Handler 3 is starting...");
+            Task.Delay(100).Wait();
+            Console.WriteLine("   Handler 3 is done.");
+        }
+    }
+
+
+    public class NaiveButton
+    {
+        public event EventHandler? Clicked;
+
+        public void Click()
+        {
+            Console.WriteLine("Somebody has clicked a button. Let's raise the event...");
+            Clicked?.Invoke(this, EventArgs.Empty);
+            Console.WriteLine("All listeners are notified.");
         }
     }
 }
