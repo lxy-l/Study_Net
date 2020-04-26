0abcf7b0634b0899a98e4b52d994cd51e20bc839
lxy <lxysac@foxmail.com>
2020-04-26 17:43:20 +0800

    特性

---
+++ ConsoleApp_Attribute/Author.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Attribute
+{
+    [AttributeUsage(AttributeTargets.Class |AttributeTargets.Struct, AllowMultiple = true)]
+    public class Author : Attribute
+    {
+        private string name;
+        public double version;
+
+        public Author(string name)
+        {
+            this.name = name;
+            version = 1.0;
+        }
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ ConsoleApp_Attribute/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp_Attribute
+{
+    [Author("NB", version = 2.0)]
+    class Program
+    {
+        private static void PrintAuthorInfo(Type t)
+        {
+            Console.WriteLine("Author information for {0}", t);
+            Attribute[] attrs = Attribute.GetCustomAttributes(t);  // 反射
+            foreach (Attribute attr in attrs)
+            {
+                if (attr is Author)
+                {
+                    Author a = (Author)attr;
+                    Console.WriteLine("   {0}, version {1:f}", a.GetName(), a.version);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintAuthorInfo(typeof(Program));
+        }
+    }
+}
