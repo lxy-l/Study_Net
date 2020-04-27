6bf9e5151f14f619b085bb6fb706ca651b97e049
lxy <lxysac@foxmail.com>
2020-04-27 16:16:38 +0800

    反射，序列化

---
+++ ConsoleApp_Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp_Reflection
+{
+    /*
+     反射提供描述程序集、模块和类型的对象（Type 类型）。 
+        可以使用反射动态地创建类型的实例，将类型绑定到现有对象，或从现有对象中获取类型，然后调用其方法或访问器字段和属性。 
+        如果代码中使用了特性，可以利用反射来访问它们。
+     反射在以下情况下很有用：
+                需要访问程序元数据中的特性时。 有关详细信息，请参阅检索存储在特性中的信息。
+                检查和实例化程序集中的类型。
+                在运行时构建新类型。 使用 System.Reflection.Emit 中的类。
+                执行后期绑定，访问在运行时创建的类型上的方法。 请参阅主题 “动态加载和使用类型”。
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using GetType to obtain type information:
+            int i = 42;
+            Type type = i.GetType();
+            Console.WriteLine(type);
+            // Using Reflection to get information of an Assembly:
+            Assembly info = typeof(int).Assembly;
+            Console.WriteLine(info);
+        }
+    }
+}
---
+++ ConsoleApp_Serialization/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApp_Serialization
+{
+    /*
+     序列化是指将对象转换成字节流，从而存储对象或将对象传输到内存、数据库或文件的过程。 它的主要用途是保存对象的状态，以便能够在需要时重新创建对象。 反向过程称为“反序列化”。
+
+    通过序列化，开发人员可以保存对象的状态，并能在需要时重新创建对象，同时还能存储对象和交换数据。 通过序列化，开发人员可以执行如下操作：
+                使用 Web 服务将对象发送到远程应用程序
+                将对象从一个域传递到另一个域
+                将对象通过防火墙传递为 JSON 或 XML 字符串
+                跨应用程序维护安全或用户特定的信息
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteXML(); ReadXML();
+        }
+
+        public static void WriteXML()
+        {
+            Book overview = new Book();
+            overview.title = "Serialization Overview";
+            System.Xml.Serialization.XmlSerializer writer =
+                new System.Xml.Serialization.XmlSerializer(typeof(Book));
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml";
+            System.IO.FileStream file = System.IO.File.Create(path);
+
+            writer.Serialize(file, overview);
+            file.Close();
+        }
+        public static void ReadXML()
+        {
+            // First write something so that there is something to read ...  
+            var b = new Book { title = "Serialization Overview" };
+            var writer = new System.Xml.Serialization.XmlSerializer(typeof(Book));
+            var wfile = new System.IO.StreamWriter(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml");
+            writer.Serialize(wfile, b);
+            wfile.Close();
+
+            // Now we can read the serialized book ...  
+            System.Xml.Serialization.XmlSerializer reader =
+                new System.Xml.Serialization.XmlSerializer(typeof(Book));
+            System.IO.StreamReader file = new System.IO.StreamReader(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "//SerializationOverview.xml");
+            Book overview = (Book)reader.Deserialize(file);
+            file.Close();
+
+            Console.WriteLine(overview.title);
+
+        }
+    }
+
+
+    public class Book
+    {
+        public String title;
+    }
+
+
+}
